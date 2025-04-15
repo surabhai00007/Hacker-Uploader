a639d90a98dfee53b39e5d167101e90bba98e233
AirPheonixSaksham <166398246+AirPheonixSaksham@users.noreply.github.com>
2025-04-16 00:31:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,3 +7,4 @@ OWNER =  6722831365
 OWNER_USERNAME = 'Saksham'
 LOG =  -100253268913
 ADMINS = [OWNER, 7161486, 181369839813]
+MONGO_URI = 'mongodb+srv://lodalesson876:FKxK7BZGq1WzE4ae@cluster0.or1gz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
