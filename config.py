03ffb0e4d9111741b31db2e2c1f81d3ac5940eb3
AirPheonixSaksham <166398246+AirPheonixSaksham@users.noreply.github.com>
2025-04-20 18:58:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,8 @@ import os
 
 api_id =  28829496
 api_hash  = '8094341dbe5bd50090de8b12a01d04b8'
-bot_token =  '7819304857:AAFCdZWw8ElfIP4Se_kHo5qbdCexQNE6cT8'
+bot_token =  '8121411999:AAHj-WZRHzzXrVFcohrAHuKrqVZ85E3GYqk'
 OWNER =  6722831365
 OWNER_USERNAME = 'Saksham'
 LOG =  -100253268913
-ADMINS = [OWNER, 7161486, 181369839813]
-MONGO_URI = 'mongodb+srv://lodalesson876:FKxK7BZGq1WzE4ae@cluster0.or1gz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+ADMINS = [OWNER, 7653433189,6722831365]
